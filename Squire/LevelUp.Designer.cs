febf5db85433d173256d3ac7d8782e346c5fc43d
Samantha Schaffer <samanthaeschaffer@gmail.com>
2013-03-07 02:23:31 +1030

    removed unecessary storing of ability scores
    and started work on pdf handling

--- Squire/LevelUp.Designer.cs
+++ Squire/LevelUp.Designer.cs
@@ -94,6 +94,8 @@
             this.classFeaturesBox = new System.Windows.Forms.ListBox();
             this.openCharacterXML = new System.Windows.Forms.OpenFileDialog();
             this.createCharacterXML = new System.Windows.Forms.SaveFileDialog();
+            this.createCharacterSheet = new System.Windows.Forms.SaveFileDialog();
+            this.openCharacterSheet = new System.Windows.Forms.OpenFileDialog();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -188,7 +190,7 @@
             this.tableLayoutPanel2.ColumnCount = 3;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.26126F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.73874F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 133F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 136F));
             this.tableLayoutPanel2.Controls.Add(this.characterFile, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.labelExplanation, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel2, 1, 1);
@@ -206,7 +208,7 @@
             // characterFile
             // 
             this.characterFile.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.characterFile.Location = new System.Drawing.Point(29, 28);
+            this.characterFile.Location = new System.Drawing.Point(27, 28);
             this.characterFile.Name = "characterFile";
             this.characterFile.Size = new System.Drawing.Size(248, 20);
             this.characterFile.TabIndex = 2;
@@ -228,7 +230,7 @@
             this.flowLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.flowLayoutPanel2.Controls.Add(this.browseButton);
             this.flowLayoutPanel2.Controls.Add(this.loadButton);
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(283, 24);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(281, 24);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(163, 29);
             this.flowLayoutPanel2.TabIndex = 5;
@@ -258,7 +260,7 @@
             // 
             this.labelLevel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.labelLevel.AutoSize = true;
-            this.labelLevel.Location = new System.Drawing.Point(497, 8);
+            this.labelLevel.Location = new System.Drawing.Point(495, 8);
             this.labelLevel.Name = "labelLevel";
             this.labelLevel.Size = new System.Drawing.Size(54, 13);
             this.labelLevel.TabIndex = 6;
@@ -269,7 +271,7 @@
             this.characterLevel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.characterLevel.Enabled = false;
             this.characterLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.characterLevel.Location = new System.Drawing.Point(499, 24);
+            this.characterLevel.Location = new System.Drawing.Point(497, 24);
             this.characterLevel.Name = "characterLevel";
             this.characterLevel.Size = new System.Drawing.Size(50, 29);
             this.characterLevel.TabIndex = 7;
@@ -685,7 +687,7 @@
             this.hitPointsPanel.ColumnCount = 3;
             this.hitPointsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.2381F));
             this.hitPointsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.76191F));
-            this.hitPointsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.hitPointsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 41F));
             this.hitPointsPanel.Controls.Add(this.labelHP, 0, 0);
             this.hitPointsPanel.Controls.Add(this.addHPButton, 2, 0);
             this.hitPointsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -702,7 +704,7 @@
             this.labelHP.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelHP.Location = new System.Drawing.Point(3, 0);
             this.labelHP.Name = "labelHP";
-            this.labelHP.Size = new System.Drawing.Size(50, 27);
+            this.labelHP.Size = new System.Drawing.Size(48, 27);
             this.labelHP.TabIndex = 6;
             this.labelHP.Text = "Hit Points";
             this.labelHP.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -712,9 +714,9 @@
             this.addHPButton.Dock = System.Windows.Forms.DockStyle.Fill;
             this.addHPButton.Enabled = false;
             this.addHPButton.Image = global::Squire.Properties.Resources.HPADDICON2;
-            this.addHPButton.Location = new System.Drawing.Point(82, 3);
+            this.addHPButton.Location = new System.Drawing.Point(79, 3);
             this.addHPButton.Name = "addHPButton";
-            this.addHPButton.Size = new System.Drawing.Size(33, 21);
+            this.addHPButton.Size = new System.Drawing.Size(36, 21);
             this.addHPButton.TabIndex = 6;
             this.addHPButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.addHPButton.UseVisualStyleBackColor = true;
@@ -922,6 +924,16 @@
             this.createCharacterXML.Filter = "XML Files|*.xml|All Files|*.*";
             this.createCharacterXML.Title = "Generate XML";
             // 
+            // createCharacterSheet
+            // 
+            this.createCharacterSheet.DefaultExt = "xml";
+            this.createCharacterSheet.Filter = "PDF Files | *.pdf |All Files|*.*";
+            this.createCharacterSheet.Title = "Generate XML";
+            // 
+            // openCharacterSheet
+            // 
+            this.openCharacterSheet.Filter = "PDF Files (*.pdf)|*pdf | All files (*.*)|*.*";
+            // 
             // LevelUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -1045,6 +1057,8 @@
         private System.Windows.Forms.TableLayoutPanel hitPointsPanel;
         private System.Windows.Forms.Label labelHP;
         private System.Windows.Forms.Button addHPButton;
+        private System.Windows.Forms.SaveFileDialog createCharacterSheet;
+        private System.Windows.Forms.OpenFileDialog openCharacterSheet;
     }
 }
 

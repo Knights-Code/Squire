5a581a571b10ac2587d739d72b0e8b501841beb0
Samantha Schaffer <samanthaeschaffer@gmail.com>
2013-04-05 13:29:43 +1030

    Fixed syntax

--- Squire/Common.cs
+++ Squire/Common.cs
@@ -7,10 +7,10 @@ namespace Squire
 {
     /// <summary>
     /// Static class for storing commonly used string literals.</summary>
-    static class Common
+    public static class Common
     {
         /// <summary>
         /// Add new strings here as you use them</summary>
-        static public string SQUIRE = "Squire";
+        public static string SQUIRE = "Squire";
     }
 }

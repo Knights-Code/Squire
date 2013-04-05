08d027550168639b24cb7f0a5f2162facac37a90
Samantha Schaffer <samanthaeschaffer@gmail.com>
2013-04-05 11:02:06 +1030

    Added class for storing string literals.
    Having a class for storing string literals rather than just having them
    appear all over the place in code simplifies changing values (whether
    because of a design change or because of a translation) and minimizes
    the chance of user error (ie: String.Equals(element, Common.STR) versus
    String.Equals(element, "STR") )
    
    While this might be a bit more work, I think using this class will
    greatly improve readability and manageability.

---
+++ Squire/Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Squire
+{
+    /// <summary>
+    /// Static class for storing commonly used string literals.</summary>
+    static class Common
+    {
+        /// <summary>
+        /// Add new strings here as you use them</summary>
+        static public string SQUIRE = "Squire";
+    }
+}
